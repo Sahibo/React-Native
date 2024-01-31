4bf88e8d26fa16ee161a30461848671e7446a998
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2024-01-31 14:23:12 +0400

    Done

---
+++ market-page/src/components/Content.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Item from '../components/Item';
+
+const Content = () => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.itemsContainer}>
+            <Text style={styles.headerText}>Hot deals</Text>
+            <View style={styles.carouselContainer}>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+            </View>
+        </View>
+
+        <View style={styles.itemsContainer}>
+            <Text style={styles.headerText}>Trending</Text>
+            <View style={styles.carouselContainer}>
+                <Item/>
+                <Item/>
+                <Item/> 
+            </View>
+        </View>        
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: "center",
+    paddingTop: 60,
+    paddingHorizontal: 16,
+    gap: 32,
+  },
+  itemsContainer: {
+    flexDirection: 'column',
+    width: '100%', 
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    gap: 16,
+  },
+  carouselContainer: {
+    flexDirection: 'row',
+    gap: 16,
+  },
+  headerText: {
+    color: "#000",
+    fontSize: 24,
+    fontStyle: "normal",
+    fontWeight: "500",
+  },
+  
+});
+
+export default Content;
---
+++ market-page/src/components/Item.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Item = () => {
+  return (
+    <View style={styles.container}>        
+       <View style={styles.imageContainer}>
+
+       </View>
+       <View style={styles.contentContainer}>
+        <Text style={styles.contentText}>Item #1 Name Goes here</Text>
+        <Text style={styles.priceText}>$19.99</Text>
+       </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: "flex-start",
+    gap: 8,
+    width: 110,
+    height: 177,
+  },
+  imageContainer: {
+    width: 110,
+    height: 110,
+    borderRadius: 8,
+    backgroundColor: '#F6F6F6'
+  },
+  contentText: {
+    color: "#000",
+    fontSize: 14,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+  priceText: {
+    color: "#000",
+    fontSize: 14,
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: 30,
+  },
+});
+
+export default Item;
